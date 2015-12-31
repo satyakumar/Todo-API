2e5233f392f16c819ee3419f7432a995a827b2cf
Satyakumar <satyakumar.kotipalli@gmail.com>
2015-12-31 11:39:17 +0530

    Add POST method for todos

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 4000;
-var todos = [
-	{
-		id: 1,
-		description: 'Learn Angular JS',
-		complete: true
-	},
-	{
-		id: 2,
-		description: 'Practive Angular JS',
-		complete: false,
-	},
-	{
-		id: 3,
-		description: 'Learn Node.JS',
-		complete: false
-	},
-	{
-		id: 4,
-		description: 'Practive Node.JS',
-		complete: false,
-	}
-	
-];
+var todoNextId = 1;
+var todos = [];
+console.log(todoNextId)
+app.use(bodyParser.json());
 app.get('/',function(req,res){
 	res.send('To Do list app');
 });
+// GET /todos
 app.get('/todos',function(req,res) {
 	res.json(todos);
 });
+// GET /todos/:id
 app.get('/todos/:id',function(req,res) {
 	var matchedId;
 	var id = parseInt(req.params.id,10);
@@ -43,6 +27,14 @@ app.get('/todos/:id',function(req,res) {
 	} else {
 		res.status(404).send();
 	}
+});
+// POST /todos
+app.post('/todos',function(req,res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	console.log(body);
+	res.json(body);
 });
 app.listen(port,function() {
 	console.log('Express server is running in the port of: '+port);
